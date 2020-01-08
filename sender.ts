dc6a321a7338d4caa0c99f952037d4a0f920d8d7
katiali <katiali@etorox.com>
2020-01-08 16:11:44 +0200

    sender - separate functions

--- sender.ts
+++ sender.ts
@@ -1,43 +1,89 @@
 #!/usr/bin/env node
 const amqp = require('amqplib/callback_api');
 
+// "amqp://user:bitnami@localhost"
+//FIXME: store all the vars in members, call to functions in the constructor 
+//TODO: how to catch errors in constructor?
+//TODO: create a function to establish all the infrastructure that will be called in the constructor
+//FIXME: types of the connection, channel
+
 export class Sender {
     q: string;
+    credentials: string;
+    connection: any;
+    channel: any;
 
-    constructor(q: string) { 
+
+    constructor(q: string, credentials: string) { 
         this.q = q; 
+        this.credentials = credentials;
+
+        this.runRabbitAndAssertQ();
     }
 
-    send = (msg: string) => {
-        
-        amqp.connect('amqp://user:bitnami@localhost', (error0, connection) => {
+    private connectionToRabbit = () : any => {
+        const connection = amqp.connect(this.credentials, (error0, connection) => {
             if (error0) {
                 console.log(error0);
                 throw error0;
             }
+            return connection;
+        })
+        return connection;
+    }
+
+    private createChannel = () : any => {
+        const channel = this.connection.createChannel((error1, channel) => {
+            if (error1) {
+                throw error1;
+            }
+            return channel;
+        })
+        return channel;
+    }
+
+    private assertQ = () : void => {
+        this.createChannel().assertQueue(this.q, {
+            durable: false
+        });
+    }
+
+    runRabbitAndAssertQ = () => {
+        this.connectionToRabbit();
+        this.createChannel();
+        this.assertQ();
+    }
+
+    send = (msg: string) => {
+        
+        // amqp.connect('amqp://user:bitnami@localhost', (error0, connection) => {
+        //     if (error0) {
+        //         console.log(error0);
+        //         throw error0;
+        //     }
 
 
-            connection.createChannel((error1, channel) => {
-                if (error1) {
-                    throw error1;
-                }
+            // connection.createChannel((error1, channel) => {
+            //     if (error1) {
+            //         throw error1;
+            //     }
                 
-                channel.assertQueue(this.q, {
-                    durable: false
-                });
+                // channel.assertQueue(this.q, {
+                //     durable: false
+                // });
                 
-                channel.sendToQueue(this.q, Buffer.from(msg));
+                this.channel.sendToQueue(this.q, Buffer.from(msg));
                 
                 console.log(`msg ${msg} sent to rabbitMQ`)
-            });
+            // });
             
-            setTimeout(function() {
-                console.log("in sender: before close");
-                connection.close();
-                console.log("in sender: after close");
+        //     setTimeout(function() {
+        //         console.log("in sender: before close");
+        //         connection.close();
+        //         console.log("in sender: after close");
                 
-            }, 500);
-        });
+        //     }, 500);
+        // });
 
     }
 }
